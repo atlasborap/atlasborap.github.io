b370f86deeb915f96623e65b720e2494b6659bfd
Atlas Borap <129795309+atlasborap@users.noreply.github.com>
2023-12-17 16:43:26 +0300

    Add files via upload

---
+++ assets/darkmode-cisco-certified-internetwork-expert.js
@@ -0,0 +1,29 @@
+let darkMode = localStorage.getItem('darkMode'); 
+
+const darkModeToggle = document.querySelector('#dark-mode-toggle');
+
+const enableDarkMode = () => {
+  document.body.classList.add('darkmode');
+  document.getElementById("changeicon").src = "../assets/light-mode.svg"
+  localStorage.setItem('darkMode', 'enabled');
+}
+
+const disableDarkMode = () => {
+  document.body.classList.remove('darkmode');
+  document.getElementById("changeicon").src = "../assets/dark-mode.svg"
+  localStorage.setItem('darkMode', null);
+}
+ 
+if (darkMode === 'enabled') {
+  enableDarkMode();
+}
+
+darkModeToggle.addEventListener('click', () => {
+  darkMode = localStorage.getItem('darkMode'); 
+  
+  if (darkMode !== 'enabled') {
+    enableDarkMode();
+  } else {  
+    disableDarkMode(); 
+  }
+});
