e0f704b1953a3623df2b326fefe77b36f5df042e
Atlas Borap <129795309+atlasborap@users.noreply.github.com>
2023-12-17 15:18:35 +0300

    Update darkmode.js

--- assets/darkmode.js
+++ assets/darkmode.js
@@ -4,13 +4,13 @@ const darkModeToggle = document.querySelector('#dark-mode-toggle');
 
 const enableDarkMode = () => {
   document.body.classList.add('darkmode');
-  document.getElementById("changeicon").innerHTML = "light_mode"
+  document.getElementById("changeicon").src = "assets/dark-mode.svg"
   localStorage.setItem('darkMode', 'enabled');
 }
 
 const disableDarkMode = () => {
   document.body.classList.remove('darkmode');
-  document.getElementById("changeicon").innerHTML = "dark_mode"
+  document.getElementById("changeicon").src = "assets/light-mode.svg"
   localStorage.setItem('darkMode', null);
 }
  
